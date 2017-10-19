369002191f49e74eeed0ba2104da5f0e3718a905
Ana Pena <anpena@csumb.edu>
2017-10-19 05:33:33 +0000

    New Version

--- index.php
+++ index.php
@@ -23,6 +23,7 @@
             
          </div>   
             <br/>
+            Select: </br>
             <select name="category" style="color:black; font-size:1.0em">
                 <option value=""> -Select One </option>
                 <option value="a" > List of Devices </option>
@@ -31,6 +32,15 @@
                 <option value="d" > Available Devices </option>
                 
             </select> </br> </br>
+            Sort by: </br>
+            <select name="sort" style="color:black; font-size:1.0em">
+                <option value=""> -Select One </option>
+                <option value="a" > Price </option>
+                <option value="b" > Name</option>
+                
+            </select> </br> </br>
+            
+            
           <input type="submit" name="submit" value="Submit">  
           
           </div>   
@@ -43,22 +53,22 @@ $servername = "us-cdbr-iron-east-05.cleardb.net";
 $username = "b910465920c218";
 $password = "9bd5a61c";
 $dbname = "heroku_b09823b21f68f71";
-
-
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 $option= $_POST["category"];
+$sort = "";
+$sort = $_POST["sort"];
+
+
 
 if ($option == ""){
-
 if ($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
 }
-
 //Make querier
+
 $sql= "SELECT deviceName FROM `device` order by deviceName ASC ";
 $result = $conn->query($sql);
-
 if ($result->num_rows > 0){
     while($row = $result->fetch_assoc()){
         echo "<h3>". "Device: " .$row["deviceName"]. "</br> </h3>";
@@ -67,24 +77,29 @@ if ($result->num_rows > 0){
 } else {
     echo "0 results";
 }
-
 $conn->close();
 }
-
-
-
-
-
 if ($option == "a"){
-
 if ($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
 }
 
-//Make querier
-$sql= "SELECT deviceName FROM `device` order by deviceName ASC ";
-$result = $conn->query($sql);
+if ($sort == "a"){
+    $sql= "SELECT deviceName, price FROM `device` order by price ASC ";
+    $result = $conn->query($sql);
+if ($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+        echo "<h3>". "Device: " .$row["deviceName"]. " Price: $". $row["price"]. "</br> </h3>";
+        
+    }
+} else {
+    echo "0 results";
+}
+}
 
+if ($sort == "b"){
+    $sql= "SELECT deviceName FROM `device` order by deviceName ASC ";
+    $result = $conn->query($sql);
 if ($result->num_rows > 0){
     while($row = $result->fetch_assoc()){
         echo "<h3>". "Device: " .$row["deviceName"]. "</br> </h3>";
@@ -93,20 +108,49 @@ if ($result->num_rows > 0){
 } else {
     echo "0 results";
 }
+}
+
+if ($sort == ""){
+$sql= "SELECT deviceName FROM `device`";
+$result = $conn->query($sql);
+if ($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+        echo "<h3>". "Device: " .$row["deviceName"]. "</br> </h3>";
+        
+    }
+} else {
+    echo "0 results";
+}
+}
+
 
 $conn->close();
 }
+
+
 
 if ($option == "b"){
-
 if ($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
 }
 
-//Make querier
+
+if ($sort=="a"){
+$sql= "SELECT deviceName, deviceType, price FROM `device` order by price ASC ";
+$result = $conn->query($sql);
+if ($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+        echo "<h3>". "Device: " .$row["deviceName"]. " Type: " .$row["deviceType"]. " Price: $". $row["price"].  "</br> </h3>";
+        
+    }
+} else {
+    echo "0 results";
+}  
+}
+
+if ($sort=="b"){
 $sql= "SELECT deviceName, deviceType FROM `device` order by deviceName ASC ";
 $result = $conn->query($sql);
-
 if ($result->num_rows > 0){
     while($row = $result->fetch_assoc()){
         echo "<h3>". "Device: " .$row["deviceName"]. " Type: " .$row["deviceType"].  "</br> </h3>";
@@ -114,57 +158,123 @@ if ($result->num_rows > 0){
     }
 } else {
     echo "0 results";
+}   
+    
+}
+
+if ($sort== ""){
+    $sql= "SELECT deviceName, deviceType FROM `device`";
+$result = $conn->query($sql);
+if ($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+        echo "<h3>". "Device: " .$row["deviceName"]. " Type: " .$row["deviceType"].  "</br> </h3>";
+        
+    }
+} else {
+    echo "0 results";
+}   
+    
 }
 
 $conn->close();
 }
 
-if ($option == "c"){
 
+if ($option == "c"){
 if ($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
 }
 
-//Make querier
-$sql= "SELECT deviceName, price FROM `device` order by deviceName ASC ";
+if ($sort == "a"){
+$sql= "SELECT deviceName, price FROM `device` order by price ASC ";
 $result = $conn->query($sql);
-
 if ($result->num_rows > 0){
     while($row = $result->fetch_assoc()){
-        echo "<h3>"."Device: " .$row["deviceName"]. " Price: $" .$row["price"].  "</br></h3>";
+        echo "<h3>"."Device: " .$row["deviceName"]. "   Price: $" .$row["price"].  "</br></h3>";
+        
+    }
+} else {
+    echo "0 results";
+}   
+}
+
+if ($sort == "b"){
+$sql= "SELECT deviceName, price FROM `device` order by deviceName ASC ";
+$result = $conn->query($sql);
+if ($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+        echo "<h3>"."Device: " .$row["deviceName"]. "   Price: $" .$row["price"].  "</br></h3>";
         
     }
 } else {
     echo "0 results";
 }
+    
+}
+
+if ($sort == ""){
+$sql= "SELECT deviceName, price FROM `device` ";
+$result = $conn->query($sql);
+if ($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+        echo "<h3>"."Device: " .$row["deviceName"]. "   Price: $" .$row["price"].  "</br></h3>";
+        
+    }
+} else {
+    echo "0 results";
+}   
+}
 
 $conn->close();
 }
-
 if ($option == "d"){
 
 if ($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
 }
 
-//Make querier
-$sql= "SELECT deviceName FROM `device` where status = 'available' order by deviceName ASC ";
+    
+if ($sort == "a"){
+ $sql= "SELECT deviceName, price FROM `device` where status = 'available' order by price ASC ";
 $result = $conn->query($sql);
-
 if ($result->num_rows > 0){
     while($row = $result->fetch_assoc()){
-        echo "<h3><div class='yy'>"."Device: "."</div>" .$row["deviceName"]."</h3>";
+        echo "<h3><div class='yy'>"."Device: "."</div>" .$row["deviceName"]. "<div class='yy'>". "Price:"."</div>". "$". $row["price"]. "</h3>";
         
     }
 } else {
     echo "0 results";
+}  
+}
+
+if ($sort == "b"){
+$sql= "SELECT deviceName FROM `device` where status = 'available' order by deviceName ASC ";
+$result = $conn->query($sql);
+if ($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+        echo "<h3><div class='yy'>"."Device: "."</div>" .$row["deviceName"]. "</h3>";
+        
+    }
+} else {
+    echo "0 results";
+}      
+}
+
+if ($sort == ""){
+$sql= "SELECT deviceName FROM `device` where status = 'available' ";
+$result = $conn->query($sql);
+if ($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+        echo "<h3><div class='yy'>"."Device: "."</div>" .$row["deviceName"]. "</h3>";
+        
+    }
+} else {
+    echo "0 results";
+}     
 }
 
 $conn->close();
 }
-
-
-
 ?>
 </body>
 </html>
